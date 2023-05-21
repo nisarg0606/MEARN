e670c1266570eaec6426d651193f53c1c81715cd
nisarg001 <106367579+nshah-capri@users.noreply.github.com>
2023-05-21 16:12:31 +0530

    updated status codes

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ exports.createUser = async (req, res) => {
   try {
     const user = new User(req.body);
     const savedUser = await user.save();
-    res.json(savedUser);
+    res.status(201).json(savedUser);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -15,7 +15,7 @@ exports.createUser = async (req, res) => {
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
-    res.json(users);
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -26,7 +26,7 @@ exports.getUserById = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     if (!user) throw Error('User not found');
-    res.json(user);
+    res.status(200).json(user);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -37,7 +37,7 @@ exports.updateUser = async (req, res) => {
   try {
     const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
     if (!user) throw Error('User not found');
-    res.json(user);
+    res.status(200).json(user);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -48,7 +48,7 @@ exports.deleteUser = async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
     if (!user) throw Error('User not found');
-    res.json({ message: 'User deleted' });
+    res.status(204).json({ message: 'User deleted' });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
